57e089bbeb89c1dd1905de1544101587230c6a30
jade-robertson <br60@st-andrews.ac.uk>
2024-10-19 12:23:25 +0100

    add list-test program

---
+++ user/list-test/src/main.cpp
@@ -0,0 +1,17 @@
+#include <stacsos/console.h>
+#include <stacsos/list.h>
+using namespace stacsos;
+
+int main(const char *cmdline)
+{
+	list<int> test_list;
+	for (int x =0; x<1000; x++){
+        test_list.append(x);
+	} 
+    while(!test_list.empty()){
+    console::get().writef("%d\n",test_list.pop());
+
+    }
+    
+	return 0;
+}
